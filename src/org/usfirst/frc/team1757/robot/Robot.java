904055359d72ca516e17342571d64b4f1c1443d9
RyanMarten <17rmarten@wpsstudents.org>
2015-11-21 19:49:34 -0500

    Deleted Line Break

--- src/org/usfirst/frc/team1757/robot/Robot.java
+++ src/org/usfirst/frc/team1757/robot/Robot.java
@@ -23,7 +23,6 @@ import edu.wpi.first.wpilibj.SpeedController;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
- * 
  */
 
 public class Robot extends IterativeRobot {
